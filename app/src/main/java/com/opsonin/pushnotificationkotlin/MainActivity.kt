b0f10aa5413a319b2210bad1c51d887ddce5dd09
project-nazmul <project.nazmul@gmail.com>
2024-08-28 16:28:38 +0600

    updated

--- app/src/main/java/com/opsonin/pushnotificationkotlin/MainActivity.kt
+++ app/src/main/java/com/opsonin/pushnotificationkotlin/MainActivity.kt
@@ -2,12 +2,13 @@ package com.opsonin.pushnotificationkotlin
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +19,10 @@ class MainActivity : AppCompatActivity() {
             val channel = NotificationChannel("1","1",NotificationManager.IMPORTANCE_DEFAULT)
             getSystemService(NotificationManager::class.java).createNotificationChannel(channel)
         }
+
+        val manager: PackageManager = applicationContext.packageManager
+        val info: PackageInfo = manager.getPackageInfo(applicationContext.packageName, 0)
+        val version = info.versionName
+        Log.d("VERSION_NAME",version)
     }
 }
